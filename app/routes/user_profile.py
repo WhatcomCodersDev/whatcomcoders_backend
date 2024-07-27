63a2db2dc52eb5d2f7a1e1fb2328e0bf9bf2e8d5
hanselguzman-soto <soto26938@gmail.com>
2024-07-26 22:33:22 -0700

    ad dmore try catch exception

--- app/routes/user_profile.py
+++ app/routes/user_profile.py
@@ -12,9 +12,7 @@ bp = Blueprint("profiles", __name__)
 
 @bp.route("/profile/<slug>", methods=["GET"])
 def get_profile_by_name(slug):
-    print(slug)
     user_data = firestore_db.get_profile_by_slug(slug)
-    print("user_data:", user_data)
     if user_data:
         return jsonify(user_data)
     else:
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,16 +1,14 @@
-from app.services.flow_manager import FlowManager
+from app.services.google_flow_manager import GoogleSignInFlowManager
 from app.services.logger_manager import LoggerManager
 from app.services.firestore_wrapper import FirestoreDBWrapper
 from app.services.sendgrid_wrapper import SendGridWrapper
-from app.utils import setup_google_auth_flow
 from google.cloud import firestore 
 from flask_jwt_extended import JWTManager
 
 jwt_manager = JWTManager()
 firestore_client = firestore.Client()
-flow = setup_google_auth_flow()
 
-flow_manager = FlowManager(flow)
+google_signin_flow_manager = GoogleSignInFlowManager()
 firestore_db = FirestoreDBWrapper(firestore_client)
 logger_manager = LoggerManager()
 sendgrid_wrapper = SendGridWrapper()
